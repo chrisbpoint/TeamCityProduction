ac1933bf0e9ce2cece53fefecc319995589baa51
Christopher Behrens <christopher.behrens@desy.de>
2019-05-06 11:23:56 +0200

    Add env var export

--- Pydoocs/Clientlib/build_clientlib.sh
+++ Pydoocs/Clientlib/build_clientlib.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DOOCSARCH=Darwin-x86_64
+export DOOCSARCH=Darwin-x86_64
 
 git -C doocs/$DOOCSARCH pull || git clone http://doocs-git.desy.de/cgit/doocs/$DOOCSARCH.git doocs/$DOOCSARCH
 git -C doocs/library/common/gul pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/gul.git doocs/library/common/gul
 git -C doocs/library/common/clientlib pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/clientlib.git doocs/library/common/clientlib
 
 cd doocs/library/common/gul && make clean && make localinstall && cd - 
-cd doocs/library/common/clientlib && pwd && ls && ls ../../../ && sed -i "" -e '/  $(OBJDIR)\/libTTFapi./d' Makefile && make clean && make localinstall
+cd doocs/library/common/clientlib && sed -i "" -e '/  $(OBJDIR)\/libTTFapi./d' Makefile && make clean && make localinstall
