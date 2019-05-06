0321c83f960c7c14a53e35fa7c2908986a143c0a
Christopher Behrens <christopher.behrens@desy.de>
2019-05-06 12:39:40 +0200

    Add actual installation

--- Pydoocs/Clientlib/build_clientlib.sh
+++ Pydoocs/Clientlib/build_clientlib.sh
@@ -9,4 +9,4 @@ git -C $DOOCS/library/common/gul pull || git clone http://doocs-git.desy.de/cgit
 git -C $DOOCS/library/common/clientlib pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/clientlib.git $DOOCS/library/common/clientlib
 
 cd $DOOCS/library/common/gul && make clean && make localinstall
-cd $DOOCS/library/common/clientlib && sed -i "" -e '/	$(OBJDIR)\/libTTFapi./d' Makefile && make clean;# && make localinstall
+cd $DOOCS/library/common/clientlib && sed -i "" -e '/	$(OBJDIR)\/libTTFapi./d' Makefile && make clean && make localinstall
