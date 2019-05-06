46dd22633732ea67974d574c5b0ef0f0a1b9e516
Christopher Behrens <christopher.behrens@desy.de>
2019-05-06 12:34:39 +0200

    Fix sed tabbing

--- Pydoocs/Clientlib/build_clientlib.sh
+++ Pydoocs/Clientlib/build_clientlib.sh
@@ -8,5 +8,5 @@ git -C $DOOCS/$DOOCSARCH pull || git clone http://doocs-git.desy.de/cgit/doocs/$
 git -C $DOOCS/library/common/gul pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/gul.git $DOOCS/library/common/gul
 git -C $DOOCS/library/common/clientlib pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/clientlib.git $DOOCS/library/common/clientlib
 
-cd $DOOCS/library/common/gul && make clean && make localinstall && cd - 
-cd $DOOCS/library/common/clientlib && sed -i "" -e '	/$(OBJDIR)\/libTTFapi./d' Makefile && make clean && make localinstall
+cd $DOOCS/library/common/gul && make clean && make localinstall
+cd $DOOCS/library/common/clientlib && sed -i "" -e '/	$(OBJDIR)\/libTTFapi./d' Makefile && make clean;# && make localinstall
