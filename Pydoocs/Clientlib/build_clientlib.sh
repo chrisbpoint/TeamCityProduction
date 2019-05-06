d9d9ffc02bf69919ebc8a73e5d9bd367194a78fb
Christopher Behrens <christopher.behrens@desy.de>
2019-05-06 12:14:19 +0200

    Clean-up build script

--- Pydoocs/Clientlib/build_clientlib.sh
+++ Pydoocs/Clientlib/build_clientlib.sh
@@ -2,9 +2,11 @@
 
 export DOOCSARCH=Darwin-x86_64
 
-git -C doocs/$DOOCSARCH pull || git clone http://doocs-git.desy.de/cgit/doocs/$DOOCSARCH.git doocs/$DOOCSARCH
-git -C doocs/library/common/gul pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/gul.git doocs/library/common/gul
-git -C doocs/library/common/clientlib pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/clientlib.git doocs/library/common/clientlib
+DOOCS=$PWD/doocs
 
-cd doocs/library/common/gul && make clean && make localinstall && cd - 
-cd doocs/library/common/clientlib && sed -i "" -e '/	$(OBJDIR)\/libTTFapi./d' Makefile && make clean && make localinstall
+git -C $DOOCS/$DOOCSARCH pull || git clone http://doocs-git.desy.de/cgit/doocs/$DOOCSARCH.git $DOOCS/$DOOCSARCH
+git -C $DOOCS/library/common/gul pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/gul.git $DOOCS/library/common/gul
+git -C $DOOCS/library/common/clientlib pull || git clone http://doocs-git.desy.de/cgit/doocs/library/common/clientlib.git $DOOCS/library/common/clientlib
+
+cd $DOOCS/library/common/gul && make clean && make localinstall && cd - 
+cd $DOOCS/library/common/clientlib && sed -i "" -e '	/$(OBJDIR)\/libTTFapi./d' Makefile && make clean && make localinstall
